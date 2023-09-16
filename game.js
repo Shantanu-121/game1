46d5ac0ef65e44836a622c556a6402cfe39a1379
Shantanu-121 <143492642+Shantanu-121@users.noreply.github.com>
2023-09-16 00:48:16 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,53 +1,48 @@
-var ttl = ["rock","paper","scissors"];
+var ttl = ["rock", "paper", "scissors"];
+const aud = new Audio("./crash.mp3");
 
-$("button").click(function(){
-    var randnum1 = Math.floor((Math.random())*3);
-var randnum2 = Math.floor((Math.random())*3);
-       document.querySelector(".player1").setAttribute("src","game"+randnum1+".png");
-       document.querySelector(".player2").setAttribute("src","game"+randnum2+".png");
-       decide (randnum1,randnum2);
-})
-function decide (num1,num2)
-{
-    if(num1===num2)
-    {
+$("button").click(function () {
+    aud.play();
+    var randnum1 = Math.floor((Math.random()) * 3);
+    var randnum2 = Math.floor((Math.random()) * 3);
+    document.querySelector(".player1").setAttribute("src", "game" + randnum1 + ".png");
+    document.querySelector(".player2").setAttribute("src", "game" + randnum2 + ".png");
+    decide(randnum1, randnum2);
+});
+function decide(num1, num2) {
+    if (num1 === num2) {
         $("h1").text("Draw , now the food must be divided by scale !");
-        $("h2").remove();
+        $("h2").text("Press start for another chance !");
+
     }
-    else if (num1===0 && num2===1)
-    {
+    else if (num1 === 0 && num2 === 1) {
         $("h1").text("Player 2 won, the food god is with him !");
-        $("h2").remove();
+        $("h2").text("Press start for another chance !");
 
     }
-    else if (num2===0 && num1===1)
-    {
+    else if (num2 === 0 && num1 === 1) {
         $("h1").text("Player 1 won, the food god is with him !");
-        $("h2").remove();
+        $("h2").text("Press start for another chance !");
 
     }
-    else if (num1===1 && num2===2)
-    {
+    else if (num1 === 1 && num2 === 2) {
         $("h1").text("Player 2 won, the food god is with him !");
-        $("h2").remove();
+        $("h2").text("Press start for another chance !");
 
     }
-    else if (num1===2 && num2===1)
-    {
+    else if (num1 === 2 && num2 === 1) {
         $("h1").text("Player 1 won, the food god is with him !");
-        $("h2").remove();
+        $("h2").text("Press start for another chance !");
 
     }
-    else if (num1===0 && num2===2)
-    {
+    else if (num1 === 0 && num2 === 2) {
         $("h1").text("Player 1 won, the food god is with him !");
-        $("h2").remove();
+        $("h2").text("Press start for another chance !");
 
     }
-    else if (num1===2 && num2===0)
-    {
+    else if (num1 === 2 && num2 === 0) {
         $("h1").text("Player 2 won, the food god is with him !");
-        $("h2").remove();
+        $("h2").text("Press start for another chance !");
 
     }
 }
